7a9bb46313f1c03cdd9fd198481816f154bcf333
Leandro-1 <leandro199908@outlook.com>
2024-06-27 23:26:07 -0300

    pequenas mudanças e inserção do cadastro de propriedade

---
+++ cadastro_propriedadeAction.php
@@ -0,0 +1,31 @@
+<?php require_once "cabecalho.php"; ?>
+<title>Cadastro de Propriedade</title>
+</head>
+<body>
+    <div class="w3-padding w3-content w3-text-grey w3-third w3-display-middle">
+        <?php
+        require_once 'conexaoBD.php';
+        if (empty($_POST["numero"]) || empty($_POST["bloco"])) {
+            echo "Por favor, preencha todos os campos!";
+            return;
+        }
+
+        $sql = "INSERT INTO propriedade (num_propriedade, bloco_quadra) VALUES ('" . $_POST['numero'] . "', '" . $_POST['bloco'] . "')";
+        if ($conexao->query($sql) === TRUE) {
+            echo '
+            <a href="cadastro_propriedade.html">
+                <h1 class="w3-button w3-black">Propriedade salva com êxito! </h1>
+            </a>
+            ';
+        } else {
+            echo '
+            <a href="index.php">
+                <h1 class="w3-button w3-black">ERRO! </h1>
+            </a>
+            ';
+        }
+        $conexao->close();
+        ?>
+    </div>
+</body>
+</html>
