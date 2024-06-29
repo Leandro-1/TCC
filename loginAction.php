35245deac80eb0faf7e335a44c4b81797535b734
Leandro-1 <leandro199908@outlook.com>
2024-06-28 23:11:25 -0300

    o codigo foi diminuido

--- loginAction.php
+++ loginAction.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-<title>Login</title>
+    <title>Login</title>
 </head>
 <body>
 
@@ -17,39 +17,31 @@ $senha = $_POST['senha'];
 
 require_once 'conexaoBD.php';
 
-
 if ($conexao->connect_error) {
     die("Connection failed: " . $conexao->connect_error);
 }
 
-$sql = "SELECT * FROM usuario WHERE login = '".$login."';";
+$sql = "SELECT * FROM usuario WHERE login = '$login';";
 $resultado = $conexao->query($sql);
 
 $linha = mysqli_fetch_array($resultado);
-if($linha != null) {
-    if($linha['senha'] == $senha && $linha['privilegio'] == 'administrador') {
-        echo '
-        <a href="menu.html">
-            <h1 class="w3-button w3-black">'. $linha['nome'] .', Seja Bem-Vindo!</h1>
-        </a>'; }
 
-        if($linha['senha'] == $senha && $linha['privilegio'] == 'operador') {
-            echo '
-            <a href="">
-                <h1 class="w3-button w3-black">'. $linha['nome'] .', Seja Bem-Vindo!</h1>
-            </a>';
-} else {
-        echo '
-        <a href="index.php">
-            <h1 class="w3-button w3-black">Login Inválido!</h1>
-        </a>';
+if ($linha && $linha['senha'] == $senha) {
+    $bemVindoMensagem = $linha['nome'] . ', Seja Bem-Vindo!';
+    if ($linha['privilegio'] == 'administrador') {
+        $link = 'menu.html';
+    } elseif ($linha['privilegio'] == 'operador') {
+        $link = '#';
+    } else {
+        $link = 'index.php';
+        $bemVindoMensagem = 'Login Inválido!';
     }
 } else {
-    echo '
-    <a href="index.php">
-        <h1 class="w3-button w3-black">Login Inválido!</h1>
-    </a>';
+    $link = 'index.php';
+    $bemVindoMensagem = 'Login Inválido!';
 }
+
+echo "<a href=\"$link\"><h1 class=\"w3-button w3-black\">$bemVindoMensagem</h1></a>";
 
 $conexao->close();
 ?>
