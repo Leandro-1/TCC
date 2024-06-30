9be4ed0bbc4d4aa0afb6acaf8544626c10c81f9e
Jéssie <jessie.neire@gmail.com>
2024-06-30 18:30:21 -0300

    criação sub_menus
    criei os sub_menus

---
+++ encomenda.php
@@ -0,0 +1,24 @@
+<?php require_once("cabecalho.php"); ?>
+
+<title>Encomenda</title>
+</head>
+
+<body class="menu-body">
+    <div class="menu-container">
+        <a href="menu.php" class="menu-item w3-red">
+            <span>Voltar Menu Principal</span><br>
+            <i class="fas fa-arrow-circle-left w3-xxlarge"></i>
+        </a>
+        <!--Criar pagina de cadastro de encomenda -->
+        <a href="" class="menu-item">
+            <span>Cadastrar Encomenda</span><br>
+            <i class="fas fa-edit w3-xxlarge"></i>
+        </a>
+        <!--Criar pagina de consulta de encomendas -->
+        <a href="" class="menu-item">
+            <span>Consultar Encomenda</span><br>
+            <i class="fas fa-search w3-xxlarge"></i>
+        </a>
+    </div>
+
+    <?php require_once("rodape.php"); ?>
---
+++ morador.php
@@ -0,0 +1,24 @@
+<?php require_once("cabecalho.php"); ?>
+
+<title>Morador</title>
+</head>
+
+<body class="menu-body">
+    <div class="menu-container">
+        <a href="menu.php" class="menu-item w3-red">
+            <span>Voltar Menu Principal</span><br>
+            <i class="fas fa-arrow-circle-left w3-xxlarge"></i>
+        </a>
+        <!--Criar pagina de cadastro de morador -->
+        <a href="" class="menu-item">
+            <span>Cadastrar Morador</span><br>
+            <i class="fas fa-edit w3-xxlarge"></i>
+        </a>
+        <!--Criar pagina de Consulta de morador -->
+        <a href="" class="menu-item">
+            <span>Consultar Morador</span><br>
+            <i class="fas fa-search w3-xxlarge"></i>
+        </a>
+    </div>
+
+<?php require_once("rodape.php"); ?>
