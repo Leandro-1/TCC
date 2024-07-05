ad2628541fb5be3097d6a7f0c4f257d2b2aca90e
Jéssie <jessie.neire@gmail.com>
2024-07-05 12:17:30 -0300

    add reset
    add reset

--- cabecalho.php
+++ cabecalho.php
@@ -7,4 +7,4 @@
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="style.css">
-   
+    <link rel="stylesheet" href="reset.css">
