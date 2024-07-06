e8b448815e2e5ae5becc57f605e3700475bb6b08
Jéssie <jessie.neire@gmail.com>
2024-07-06 15:32:20 -0300

    Update conexaoBD.php

--- conexaoBD.php
+++ conexaoBD.php
@@ -1,20 +1,24 @@
 <?php
 // BD para testes PC Leandro
-$servername = "localhost";
+/*$servername = "localhost";
 $username = "root";
 $password = "conpac@1";
 $dbname = "conpac";
 $conexao = new mysqli($servername, $username, $password, $dbname);
 if ($conexao->connect_error) {
-die("Connection failed: " . $conexao->connect_error);
+die("Connection failed: " . $conexao->connect_error); 
+*/
 
 
  //BD temporario teste Jéssie
- //$servername = "localhost:3307";
-// $username = "root";
- //$password = "usbw";
- //$dbname = "conpac";
-// $conexao = new mysqli($servername, $username, $password, $dbname);
-
+ $servername = "localhost:3307";
+$username = "root";
+ $password = "usbw";
+ $dbname = "conpac";
+$conexao = new mysqli($servername, $username, $password, $dbname);
+if ($conexao->connect_error) {
+    die("Connection failed: " . $conexao->connect_error); 
 }
+
+
 ?>
