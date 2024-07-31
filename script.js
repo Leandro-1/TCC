7bf5d6f8b2aa896bd89e834fb4b26074e7ae8402
Jéssie <jessie.neire@gmail.com>
2024-07-31 16:50:13 -0300

    .

--- script.js
+++
@@ -1,9 +0,0 @@
-function exibirModal(mensagem) {
-    document.getElementById('mensagemModal').textContent = mensagem;
-    document.getElementById('myModal').style.display = 'block';
-}
-
-// Função para fechar o modal
-function fecharModal() {
-    document.getElementById('myModal').style.display = 'none';
-}
