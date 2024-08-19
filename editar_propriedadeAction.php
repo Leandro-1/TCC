45f914f1d83c26344b7b2d16770a61ee87fac94c
Jéssie <jessie.neire@gmail.com>
2024-08-19 17:08:55 -0300

    up na pag de direcionamento

--- editar_propriedadeAction.php
+++ editar_propriedadeAction.php
@@ -5,27 +5,26 @@
 
 <body>
 
-    <!-- centralizar -->
     <div class="w3-padding w3-content w3-text-grey w3-third w3-display-middle">
         <?php
         require_once 'conexaoBD.php';
-
-        
-
 
         $sql = "UPDATE propriedade SET num_propriedade = '" . $_POST['txtNumero'] . "', bloco_quadra='" . $_POST['txtBloco'] . "' WHERE id_propriedade =" . $_POST['txtCodigo'] . ";";
 
 
         if ($conexao->query($sql) === TRUE) {
-            echo '<a href="consultar_propriedade.php">
+            
+            echo '<a href="inicial_adm.php">
                     <h1 class="w3-button w3-black w3-center">Propriedade Atualizada com Sucesso! </h1>
-                </a>';
+              </a> ';
+            
             $id = mysqli_insert_id($conexao);
         } else {
-            echo '<a href="consultar_propriedade.php">
+            echo '<a href="inicial_adm.php">
                     <h1 class="w3-button w3-black w3-center">ERRO... Tente Novamente! </h1>
                 </a>';
         }
+   
         $conexao->close();
         ?>
     </div>
