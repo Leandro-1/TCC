982236b03a2cbdf14a1b96d89d66ea456ac909be
Jéssie <jessie.neire@gmail.com>
2024-08-20 14:04:49 -0300

    up na mensagem

--- acessonegado.php
+++ acessonegado.php
@@ -1,17 +1,22 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <title>Acesso Negado</title>
+    <script src="script.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <title>Acesso Negado!</title>
 </head>
+
 <body>
-    <div class="w3-padding w3-content w3-text-grey w3-third w3-display-middle">
-        <h1 class="w3-button w3-black">Acesso Negado</h1>
-        <p>Você não tem permissão para acessar esta página.</p>
-        <a href="login.html" class="w3-button w3-black">Voltar para o login</a>
+    <div class="w3-padding w3-container w3-third w3-display-middle w3-red w3-text-black w3-center">
+        <h1><b>Acesso Negado!</b></h1>
+        <p>Você não tem permissão para acessar esta página.</p><br>
+        <a href="login.html" class="w3-button w3-black w3-round-xlarge">Voltar para o login</a>
     </div>
 </body>
+
 </html>
