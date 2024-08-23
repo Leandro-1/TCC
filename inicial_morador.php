a1fe6f643e196bf1875b5b5fe3aeabb189b1571a
Jéssie <jessie.neire@gmail.com>
2024-08-23 20:42:19 -0300

    up caixa de detalhes.

--- inicial_morador.php
+++ inicial_morador.php
@@ -1,20 +1,16 @@
 <?php
-//2 primeiras linhas: verificaçaõ unica de acesso para todos os usuario, chamando somente a funcao e colocando o tipo de usuario
-//podendo assim excluir o arquivos 'verificar acesso adm e todos'
 require_once('verificar_permissaoAcesso.php');
 verificar_permissao('morador');
 require_once('cabecalho.php');
 require_once('conexaoBD.php');
+
 ?>
-
-
-<!-- Falta a criação da logica para o usuario verificar somente suas entregas -->
 
 <div class="w3-container " style="margin-top: 80px; padding-bottom: 4rem;">
 
     <div class="w3-bar aba_menu">
         <div class="abas w3-blue-grey">
-            <button class="w3-bar-item w3-button tablink  w3-right w3-red" onclick="openMenu(event,'entregas')"><b>Entregas</b></button>
+            <button class="w3-bar-item w3-button tablink w3-right"><b>Entregas</b></button>
         </div>
     </div>
 
@@ -26,20 +22,23 @@ require_once('conexaoBD.php');
         <br>
 
         <!-- Consulta de Entregas -->
-        <table class="w3-table-all w3-centered w3-hoverable" style="overflow-y:auto; ">
+        <table class="w3-table-all w3-centered w3-hoverable" style="overflow-x:auto; ">
             <tr class="w3-center w3-blue-grey">
+                <th>Apartamento/Bloco</th>
                 <th>Data Recebimento</th>
-                <th>Quem Recebeu</th>
-                <th>Morador</th>
-                <th>Relatório</th> <!-- Dentro de relatório detalhar quem retirou, e mais detalhes cadastrados -->
+                <th>Recebido por</th>
+                <th>Destinatário</th>
+                <th>Retirado por</th>
+                <th>Data Retirada</th>
                 <th>Status</th>
+                <th>Relatório</th><!-- Dentro de relatório com mais detalhes cadastrados -->
             </tr>
             <?php
             $usuario_id = $_SESSION['id_user'];
             $tipo = $_SESSION['privilegio'];
 
             // Identificar a propriedade associada ao usuário logado
-            $sql_propriedade = "SELECT id_propriedade FROM morador WHERE id_usuario = '$usuario_id'";
+            $sql_propriedade = "SELECT id_propriedade FROM morador WHERE id_usuario = $usuario_id;";
             $result_propriedade = $conexao->query($sql_propriedade);
 
             if ($result_propriedade->num_rows > 0) {
@@ -49,27 +48,55 @@ require_once('conexaoBD.php');
                 // Buscar entregas para o apartamento identificado
                 $sql_entregas = "SELECT entrega.*, propriedade.* FROM entrega 
                      JOIN propriedade ON entrega.id_residencia = propriedade.id_propriedade 
-                     WHERE propriedade.id_propriedade = '$id_propriedade'";
+                     WHERE propriedade.id_propriedade = $id_propriedade;";
                 $result_entregas = $conexao->query($sql_entregas);
 
                 while ($linha = $result_entregas->fetch_assoc()) {
-                        echo '<tr class=w3-text-black>';
-                        echo '<td>' . $linha['data_recebimento'] . '</td>';
-                        echo '<td>' . $linha['nome_morador'] . '</td>';
-                        echo '<td>' . $linha['num_propriedade'] . '</td>';
-                        echo '<td>' . $linha['bloco_quadra'] . '</td>';
-                        echo '<td>' . $linha['status'] . '</td>';
+                    echo '<tr class="w3-text-black">';
+                    echo '<td>' . $linha['num_propriedade'] . ' / ' . $linha['bloco_quadra'] . '</td>';
+                    echo '<td>' . $linha['data_recebimento'] . '</td>';
+                    echo '<td>' . $linha['recebido_por'] . '</td>';
+                    echo '<td>' . $linha['nome_morador'] . '</td>';
+                    echo '<td>' . $linha['retirado_por'] . '</td>';
+                    echo '<td>' . $linha['data_retirada'] .  '</td>';
+                    echo '<td>' . $linha['status'] . '</td>';
 
-                        // criar um modal para relatório com muito mais dados e detalhado
-                        echo '<td><a href="relatorio_entrega.php?dt_recebimento=' . $linha['data_recebimento'] . '&nome=' . $linha['nome_morador'] . '&num_apart=' . $linha['num_propriedade'] . '&bloco=' . $linha['bloco_quadra'] . '&status=' . $linha['status'] . '">
-                                        <i class="fa fa-user-times w3-large w3-text-black"></i> 
-                                    </a></td>
-                            </td>';
-                    }
+                    // colocar mais detalhes relevantes
+                    echo '<td><button onclick="detalhesEntrega(\'' . $linha['data_recebimento'] . '\',\'' . $linha['recebido_por'] . '\',\'' . $linha['nome_morador'] . '\',\'' . $linha['status'] . '\')" class="w3-text-blue">Detalhes</button></td>';
+                }
             }
             ?>
         </table><br>
 
+        <!-- modal para detalhes da entrega -->
+        <div id="detalhes_entrega" class="w3-modal">
+            <div class="w3-modal-content">
+                <div class="w3-container">
+                    <span onclick="document.getElementById('detalhes_entrega').style.display='none'" class="w3-button w3-display-topright w3-hover-red w3-large"><b>&times;</b></span>
+                    <div class="w3-container">
+                        <h2>Detalhes da Entrega</h2>
+                        <span>Data de Recebimento:</span>
+                        <p id="dt_receb"></p>
+                        <span>Recebido Por:</span>
+                        <p id="recebido"></p>
+                        <span>Destinatário</span>
+                        <p id="destin"></p>
+                        <span>Status</span>
+                        <p id="status_atual"></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script>
+            function detalhesEntrega(dt_recebido, recebido_por, destinatario, status) {
+                document.getElementById('dt_receb').value = dt_recebido;
+                document.getElementById('recebido').value = recebido_por;
+                document.getElementById('destin').value = destinatario;
+                document.getElementById('status_atual').value = status;
+                document.getElementById('detalhes_entrega').style.display = 'block';
+
+            }
+        </script>
     </div>
 </div><br><br>
 
