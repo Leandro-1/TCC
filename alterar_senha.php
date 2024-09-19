07142097710b6d6cd8414a77436eb412ef84f58f
Jéssie <jessie.neire@gmail.com>
2024-09-19 14:01:00 -0300

    criação da pagina de alterar senha

---
+++ alterar_senha.php
@@ -0,0 +1,30 @@
+<?php
+require_once('cabecalho.php');
+session_start();
+?>
+
+
+<div class="w3-container w3-display-middle">
+    <h1>Alterar Senha</h1>
+    <form id="myForm_alterar_senha" action="alterar_senhaAction.php" method="post">
+        
+        <label>Login:</label><br>
+        <input type="text" id="login_antigo" required><br>
+
+        <label>Senha Atual:</label><br>
+        <input type="text" id="senha_antiga" required><br>
+
+        <label>Nova Senha:</label><br>
+        <input type="text" id="senha_nova" required><br>
+        <label>Confirmar Senha:</label><br>
+        <input type="text" id="confirma_senha" required><br>
+
+        <button class="w3-button">Confirmar</button>
+
+    </form>
+
+</div>
+
+<?php
+require_once('rodape.php');
+?>
