a78609917ae030c07b76f2ae7516a8b306f46a15
Jéssie <jessie.neire@gmail.com>
2024-09-20 17:46:08 -0300

    .

--- alterar_senha.php
+++ alterar_senha.php
@@ -1,30 +1,28 @@
 <?php
 require_once('cabecalho.php');
-session_start();
+
 ?>
 
-
-<div class="w3-container w3-display-middle w3-card-4 w3-padding w3-round-xlarge">
-    <h1>Alterar Senha</h1>
+<div class="alterar_senha w3-container w3-card-4 w3-padding w3-round-xlarge w3-blue-grey" style="width: 400px; margin:auto; margin-top: 100px;">
+    <h1><i class="fas fa-edit"></i> <b>Alterar Senha</b></h1>
     <form id="myForm_alterar_senha" action="alterar_senhaAction.php" method="post">
-        
-        <label>Login:</label><br>
-        <input type="text" id="login_antigo" required><br>
+        <div class="w3-container">
+            <label>Login:</label><br>
+            <input class="w3-input w3-round-xlarge" type="text" name="login_alterarSenha" required>
+            <label>Senha Atual:</label><br>
+            <input class="w3-input w3-round-xlarge" type="password" name="senha_atual" required><br><br>
 
-        <label>Senha Atual:</label><br>
-        <input type="text" id="senha_antiga" required><br>
+            <label>Nova Senha:</label><br>
+            <input class="w3-input w3-round-xlarge" type="password" name="nova_senha" required>
+            <label>Confirmar Senha:</label><br>
+            <input class="w3-input w3-round-xlarge" type="password" name="confirmar_senha" required><br>
 
-        <label>Nova Senha:</label><br>
-        <input type="text" id="senha_nova" required><br>
-        <label>Confirmar Senha:</label><br>
-        <input type="text" id="confirma_senha" required><br>
-
-        <button class="w3-button">Confirmar</button>
-
+            <button type="submit" class="w3-button w3-round-xlarge" style="background-color: #101031;">Confirmar</button>
+            <i class="fa-solid fa-house w3-xxlarge w3-right" style="color: #101031;"></i>
+        </div>
     </form>
-
 </div>
-
+<div class="feedbackMessage"></div>
 <?php
 require_once('rodape.php');
 ?>
